2a729334a96848404e7153e4c754669757107562
Luiz Eduardo Rangel da Silva Abranches <luizeduardo.abranches@gmail.com>
2024-06-15 18:52:39 -0300

    Add files via upload

---
+++ authenticateApi/src/test/java/com/example/authenticateApi/AuthenticateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticateApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
