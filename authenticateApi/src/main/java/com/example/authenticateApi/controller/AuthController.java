d760a1f4b5865ab45bb8d9bd584481830698bf17
Luiz Eduardo Rangel da Silva Abranches <luizeduardo.abranches@gmail.com>
2024-06-16 15:23:16 -0300

    Add files via upload

---
+++ authenticateApi/src/main/java/com/example/authenticateApi/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.example.authenticateApi.controller;
+import com.example.authenticateApi.model.LoginRequest;
+import com.example.authenticateApi.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        String token = authService.generateToken(request.getUsername());
+        return token;
+    }
+
+    @GetMapping("/username/{token}")
+    public String extractUsername(@PathVariable String token) {
+        String username = authService.extractUsername(token);
+        return username;
+    }
+
+    // Qualquer um irá acessar
+    @GetMapping("/comum")
+    public String getComum(Authentication authentication) {
+        return "Usuário comum: " + authentication.getName();
+    }
+
+    // Somente o admin irá acessar
+    @Secured("ADMIN")
+    @GetMapping("/admin")
+    public String onlyAdmin(Authentication authentication) {
+        return "Usuário admin: " + authentication.getName();
+    }
+
+    // Somente o moderador irá acessar
+    @Secured("MODERADOR")
+    @GetMapping("/moderador")
+    public String onlyMod(Authentication authentication) {
+        return "Usuário moderador: " + authentication.getName();
+    }
+}
---
+++ authenticateApi/src/test/java/com/example/authenticateApi/AuthenticateApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.authenticateApi;
+
+import com.example.authenticateApi.application.AuthenticateApiApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AuthenticateApiApplication.class)
+class AuthenticateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
